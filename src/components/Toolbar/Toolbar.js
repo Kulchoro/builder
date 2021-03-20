25ab44a3929f3b39055a1f58d1198fb46e7725cb
Kulchoro <jumalievkulchoro@gmail.com>
2021-03-19 14:39:47 -1200

    import Navogation and fixed problem for deploy

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,8 +1,9 @@
-import classes from "Toolbar.module.css";
+import classes from "./Toolbar.module.css";
+import Navigation from "../Navigation/Navigation";
 
 const Toolbar = () => {
     return ( <div className={classes.Toolbar}>
-
+        <Navigation />
     </div> );
 }
  
