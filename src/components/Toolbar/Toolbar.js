e0d223823c568a9604a673f9d198764bd622a7ce
Kulchoro <jumalievkulchoro@gmail.com>
2021-03-19 15:18:03 -1200

    import Logo component

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,9 +1,11 @@
 import classes from "./Toolbar.module.css";
 import Navigation from "../Navigation/Navigation";
+import Logo from "../Logo/Logo";
 
 const Toolbar = () => {
     return ( <div className={classes.Toolbar}>
         <Navigation />
+        <Logo />
     </div> );
 }
  
