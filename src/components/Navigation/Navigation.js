e5705aaaf1b0bbd617fb83dd56387fb3951ce2fe
Kulchoro <jumalievkulchoro@gmail.com>
2021-03-19 14:40:51 -1200

    Creating Navigation componet

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,8 @@
+import classes from "./Navigation.module.css";
+const Navigation = () => {
+    return ( <div className={classes.Navigation}>
+        Nav
+    </div> );
+}
+ 
+export default Navigation;
