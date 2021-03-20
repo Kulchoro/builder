00b55103650dda16fa775533b1560b77f0f6fa4d
Kulchoro <jumalievkulchoro@gmail.com>
2021-03-19 15:19:10 -1200

    Creating Logo component and Logo CSS

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import classes from "./Logo.module.css";
+
+const Logo = () => {
+    return ( <div className={classes.Logo}>
+        <img src="#" alt="internetshop logo" />
+    </div> );
+}
+ 
+export default Logo;
