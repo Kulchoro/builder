45bbc90eff6284c7a022c9c272e8417178a6e43e
Kulchoro <jumalievkulchoro@gmail.com>
2021-03-23 12:13:55 -1200

    Fixed bug with drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,8 +5,8 @@ import {useState} from "react";
 const Layout = ({children}) => {
     const [openDrawer, setDrawerOpen]=useState(false);
     return ( <div className={classes.Layout}>
-        <Drawer open={openDrawer} setOpen={()=>setDrawerOpen(false)} />
         <Toolbar  openDrawer={() => setDrawerOpen(true)} />
+        <Drawer open={openDrawer} setOpen={()=>setDrawerOpen(false)} />
         {children}
     </div>);
 }
