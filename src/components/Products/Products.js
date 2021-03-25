ae30659a17271910d45ff62f962089624ef5785d
Kulchoro <jumalievkulchoro@gmail.com>
2021-03-25 11:03:27 -1200

    Created product items

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,6 +2,12 @@
 import classes from "./Products.module.css";
 
 const Products = () => {
+    const products =[
+        {productName: "Example1", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", price: 999},
+        {productName: "Example2", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", price: 888},
+        {productName: "Example3", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", price: 777},
+        {productName: "Example3", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", price: 666}
+    ]
     return ( <div className={classes.Products}>
 
     </div> );
