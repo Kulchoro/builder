0da0a0f824a99f2e8d332ade0890a63e33fe37f4
Kulchoro <jumalievkulchoro@gmail.com>
2021-03-25 11:17:00 -1200

    Changed classes in method map

--- src/components/Products/ProductsItem/ProductItem.js
+++ src/components/Products/ProductsItem/ProductItem.js
@@ -2,14 +2,14 @@ import classes from "./ProductItem.module.css";
 
 const ProductItem = ({products}) => {
     let productsItem = products.map(product => {
-        return <div> 
+        return <div className={classes.ProductItem}> 
         <div>{product.productName}</div>
         <div>{product.description}</div>
         <div>{product.price}</div>
         </div>
 
     })
-    return ( <div className={classes.ProductItem}>
+    return ( <div>
         {productsItem}
     </div> );
 }
