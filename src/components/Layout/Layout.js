c03ae6c570c1ff95c1e5b551f71ab9e66d2025e7
Kulchoro <jumalievkulchoro@gmail.com>
2021-03-25 11:02:50 -1200

    Imported Product component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,14 @@ import Drawer from "../Drawer/Drawer";
 import classes from "./Layout.module.css";
 import Toolbar from "../Toolbar/Toolbar";
 import {useState} from "react";
+import Products from "../Products/Products";
 const Layout = ({children}) => {
     const [openDrawer, setDrawerOpen]=useState(false);
     return ( <div className={classes.Layout}>
         <Toolbar  openDrawer={() => setDrawerOpen(true)} />
         <Drawer open={openDrawer} setOpen={()=>setDrawerOpen(false)} />
         {children}
+        <Products />
     </div>);
 }
  
