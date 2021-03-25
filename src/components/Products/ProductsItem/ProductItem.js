d0682d5d10000a0aba7558b294702be4c35b164f
Kulchoro <jumalievkulchoro@gmail.com>
2021-03-25 11:04:03 -1200

    Creating ProductItem component

---
+++ src/components/Products/ProductsItem/ProductItem.js
@@ -0,0 +1,9 @@
+import classes from "./ProductItem.module.css";
+
+const ProductItem = () => {
+    return ( <div className={classes.ProductItem}>
+
+    </div> );
+}
+ 
+export default ProductItem;
