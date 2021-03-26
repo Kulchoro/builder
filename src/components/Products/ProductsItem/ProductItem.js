db2aa1c6b4a5869a9333f3864df04e0400da6e03
Kulchoro <jumalievkulchoro@gmail.com>
2021-03-25 12:29:15 -1200

    Import ProductControls component and rename class name

--- src/components/Products/ProductsItem/ProductItem.js
+++ src/components/Products/ProductsItem/ProductItem.js
@@ -1,15 +1,17 @@
+import ProductControls from "../ProductControls/ProductControls";
 import classes from "./ProductItem.module.css";
 
 const ProductItem = ({products}) => {
     let productsItem = products.map(product => {
-        return <div className={classes.ProductItem}> 
+        return <div className={classes.ProductItems}> 
         <div>{product.productName}</div>
         <div>{product.description}</div>
         <div>{product.price} cent</div>
+        <ProductControls />
         </div>
 
     })
-    return ( <div className={classes.ProductItems}>
+    return ( <div className={classes.ProductItem}>
         {productsItem}
     </div> );
 }
